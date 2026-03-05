46b889b9548a51085cadd76c64b73781ccc07d3b
Corentin Gelgon <cgelgon@f4r11s15.clusters.42paris.fr>
2026-03-05 11:58:11 +0100

    Inception\: changed setup sh et compose yml

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+
+# /run/secrets/ est monté par Docker
+
 DB_PASSWORD=$(cat /run/secrets/db_password)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
